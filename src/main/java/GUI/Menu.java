f415df0367bd10b09ee24b90d4fb52f10f0a7617
Swapnil Kumbhar <SwapnilKumbhar@users.noreply.github.com>
2021-12-02 18:02:53 -0700

    feat(Menu): Added game explanation (#4)

--- src/main/java/GUI/Menu.java
+++ src/main/java/GUI/Menu.java
@@ -13,7 +13,38 @@ public class Menu extends JMenuBar {
     public JMenuItem jExp, jAbout, jCode;
     public JMenuItem jP1, jP2, jP3;
     public JMenuItem jStart, JStop, JNext, JSpeed;
-    public JFrame jframe;
+
+    //public String explanationText = "<html><ul><li>First</li><li>Second</li></ul></html>";
+   public String explanationText = """
+<html>
+<body>
+    <section>
+        <h2>Game of Life rules</h2>
+        <summary>For a space that is populated:</summary>
+        <ol>
+                  <li>Each cell with one or no neighbors dies, as if by solitude.</li>
+                  <li>Each cell with four or more neighbors dies, as if by overpopulation.</li>
+                  <li>Each cell with two or three neighbors survives.</li>
+        </ol>
+        <summary>For a space that is empty or unpopulated:</summary>
+        <ol>
+             <li>Each cell with three neighbors becomes populated.</li>
+         </ol>
+    </section>
+    <section>
+        <p><b>The Controls<b></p>
+	<ul>
+        <li>Click on various cells of the grid to initialise the living cells in Generation 0</li>
+        <li>Click on Game->Start to start the game</li>
+        <li>Click on Game->Stop to stop the game</li>
+        <li>Click on Game->Next to step one generation</li>
+        <li>Click on Game->Speed to change the speed of generations</li>
+	<ul>
+    </section>
+
+</body>
+</html>
+            """;
 
     public Menu()
     {
@@ -45,15 +76,11 @@ public class Menu extends JMenuBar {
         jGame.add(JNext);
         jGame.add(JSpeed);
         jExp.addActionListener(new ActionListener() {
-            //File pdfFile = new File("Desktop\\script.txt");
             @Override
             public void actionPerformed(ActionEvent e) {
-                Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-                try {
-                    desktop.browse(new URI("https://tinyurl.com/5bw8pt63"));
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, "Source is available on GitHub at:\nhttps://github.com/SwapnilKumbhar/CSE-563-GameOfLife", "Source", JOptionPane.INFORMATION_MESSAGE);
-                }
+                // Show the explanation pane
+                String finalExplanationHTML = explanationText.replace('\n',' ');
+                JOptionPane.showMessageDialog(null, finalExplanationHTML);
             }
         });
         jAbout.addActionListener(new ActionListener() {
